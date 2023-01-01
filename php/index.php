b2d016700e5b2599fd28e4f2acb7cb96159e8926
TheMightyPengu <97791860+TheMightyPengu@users.noreply.github.com>
2023-01-01 19:27:39 +0200

    Added turns.
    Now the user can only play if its his turn.
    Made some Small visual changes as well.

--- php/index.php
+++ php/index.php
@@ -9,17 +9,12 @@ $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $GLOBALS['input'] = json_decode(file_get_contents('php://input'), true);
 session_start();
 
-
-
 switch ($request[0]) {
 
     case 'GetPlayersHand':
         if ($method == 'GET') {
-  
             $pid = FromTokenToPid();
-            //echo($pid);
             print ShowPlayerHand($pid);
-
         } else {
             header("HTTP/1.1 400 Bad Request");
             print json_encode(['errormesg' => "Method $method not allowed here."]);
@@ -39,16 +34,16 @@ switch ($request[0]) {
 
             switch ($result['result']) {
                 case 1:
-                  print json_encode(['errormesg' => "Max players"]);
-                  exit;
+                print json_encode(['errormesg' => "Max players"]);
+                exit;
                 case 2:
-                  print json_encode(['errormesg' => "Username exists"]);
-                  exit;
+                print json_encode(['errormesg' => "Username exists"]);
+                exit;
                 case 3:
-                  print json_encode(['errormesg' => "Player already exists"]);
-                  exit;
+                print json_encode(['errormesg' => "Player already exists"]);
+                exit;
                 default:
-                  print json_encode(['success'=>"Players added"]);
+                print json_encode(['success'=>"Player added"]);
                 }
             
         } else {
@@ -58,37 +53,43 @@ switch ($request[0]) {
     break;
 
     case 'Play':
-        if ($method == 'POST') {
-
-            $pid = FromTokenToPid();
-           // echo $pid;
-            
-            $result = PlayCards($pid);
-            //echo $result;   
-
-            if ($result == 1) {
-                print json_encode(['errormesg' => "Cards not in player's hand"]);
-                exit;
-            }
-            
-        } else {    
+        $pid = FromTokenToPid();
+        $result = CanIPlay($pid);
+        if ($result == 0){
+        
             header("HTTP/1.1 400 Bad Request");
-            print json_encode(['errormesg' => "Method $method not allowed here."]);
-        }
+            print json_encode(['errormesg' => "Not your turn."]);
+            
+        } else {
+            if ($method == 'POST') {
+                $result = PlayCards($pid);
+                if ($result == 1) {
+                    print json_encode(['errormesg' => "Cards not in player's hand"]);
+                    exit;
+                }
+            } else {    
+                header("HTTP/1.1 400 Bad Request");
+                print json_encode(['errormesg' => "Method $method not allowed here."]);
+            }
+        }    
     break;
 
     case 'Objection':
         if ($method == 'POST') {
 
         $pid = FromTokenToPid();    
-        $result = iObject($pid);
+        $result = iObject($pid);    
             
         if ($result == 1) {
             print json_encode(['bluff' => 'TRUE']);
-            echo 'He gets all the cards back :)';
-        } else {
+            echo "\n He gets all the cards back :)";
+        } else if ($result == 0){
             print json_encode(['bluff' => 'FALSE']);
-            echo 'You get all the cards :(';
+            echo "\n You get all the cards :(";
+        }
+        else{
+            print json_encode(['bluff' => NULL]);
+            echo "\n You didnt object!";
         }
 
         } else {    
@@ -96,62 +97,64 @@ switch ($request[0]) {
             print json_encode(['errormesg' => "Method $method not allowed here."]);
         }
     break;
-
-////////////////////////////////ONLY FOR TESTING///////////////////////////////////////////////
+    ////////////////////////////////ONLY FOR TESTING///////////////////////////////////////////////
     case 'Vroom':
-        if ($method == 'POST') {
-
-        BroomTheBoard();
-            
-        } else {    
-            header("HTTP/1.1 400 Bad Request");
-            print json_encode(['errormesg' => "Method $method not allowed here."]);
-        }
+        if ($method == 'POST') {BroomTheBoard();}
     break;
 
     case 'Prepare':
-        if ($method == 'POST') {
-
-        PrepareNewGame();
-            
-        } else {    
-            header("HTTP/1.1 400 Bad Request");
-            print json_encode(['errormesg' => "Method $method not allowed here."]);
-        }
+        if ($method == 'POST') {PrepareNewGame();} 
     break;
 
     case 'Winner':
         if ($method == 'GET') {
+            $result = CheckForWinner();
 
-        $result = CheckForWinner();
-        echo($result);
-
-        switch ($result) {
-            case 1:
-                print json_encode(['winner' => '1']);
-                break;
-            case 2:
-                print json_encode(['winner' => '2']);
-                break;
-            case 3:
-                print json_encode(['winner' => '3']);
-                break;
-            case 4:
-                print json_encode(['winner' => '4']);
-                break;
-            default:
-                print json_encode(['winner' => '0']);
-        }
-
-        } else {    
-            header("HTTP/1.1 400 Bad Request");
-            print json_encode(['errormesg' => "Method $method not allowed here."]);
+            switch ($result) {
+                case 1:
+                    print json_encode(['winner' => '1']);
+                    break;
+                case 2:
+                    print json_encode(['winner' => '2']);
+                    break;
+                case 3:
+                    print json_encode(['winner' => '3']);
+                    break;
+                case 4:
+                    print json_encode(['winner' => '4']);
+                    break;
+                default:
+                    print json_encode(['winner' => '0/kanis']);
+            }
         }
     break;
-////////////////////////////////////////////////////////////////////////////////////
-default:
-header("HTTP/1.1 404 Not Found");
-print json_encode(['errormesg' => "Not found."]);
-break;    
+
+    case 'PlayedLast':
+        if ($method == 'GET') {
+
+            $result = CheckWhoPlayedLast();
+            //echo($result);
+
+            switch ($result) {
+                case 1:
+                    print json_encode(['last' => '1']);
+                    break;
+                case 2:
+                    print json_encode(['last' => '2']);
+                    break;
+                case 3:
+                    print json_encode(['last' => '3']);
+                    break;
+                case 4:
+                    print json_encode(['last' => '4']);
+                    break;
+            }
+        }    
+    break;
+    ////////////////////////////////////////////////////////////////////////////////////
+    default:
+        header("HTTP/1.1 404 Not Found");
+        print json_encode(['errormesg' => "Not found."]);
+        break;
 }
 ?>
